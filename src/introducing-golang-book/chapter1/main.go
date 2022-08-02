038f6fc7090ed370b817686d825d679ed77e485f
JeanCTSilva <tsjeancarlo@gmail.com>
2022-08-02 18:34:50 -0300

    finished chapter 1

--- src/introducing-golang-book/chapter1/main.go
+++ src/introducing-golang-book/chapter1/main.go
@@ -1,11 +1,3 @@
-/*
-Create a new executable program that references the fmt library
-and contains one function called main. That function takes no
-arguments and doesn’t return anything. It accesses the Println
-function contained inside of the fmt package and invokes it using
-one argument—the string Hello, World.
-*/
-
 // package declaration - must be in the beginning
 package main
 
@@ -16,6 +8,11 @@ import "fmt"
 func main() {
 	// print line = Hello World
 	fmt.Println("Hello World")
+	chapter1_exercise1()
+	chapter1_exercise2()
+	chapter1_exercise3()
+	chapter1_exercise4()
+	chapter1_exercise5()
 }
 
 /*
@@ -23,21 +20,37 @@ The name main is special because it’s the function
 that gets called when you execute the program
 */
 
-// EXERCISES
+func chapter1_exercise1() {
+	// Questions
+	fmt.Println("\n1. What is whitespace?")
+	// Answer
+	fmt.Println("Ans.: Whitespaces are the represetation of newlines, spaces and tabs. Which we cannot see.")
+}
 
-/*
-1. What is whitespace?
-R. Whitespaces are the represetation of newlines,
-spaces and tabs. Which we cannot see.
-*/
+func chapter1_exercise2() {
+	// Questions
+	fmt.Println("\n2. What is a comment? What are the two ways of writing a comment?")
+	// Answer
+	fmt.Println("Ans.: Comment is a way to make the command be ignored by the program. The two ways to write a comment are: //SINGLE LINE COMMENT /* MULTI LINE COMMENT */")
+}
 
-/*
-2. What is a comment? What are the two ways of writing a comment??
-R. Comment is a way to make the command be ignored by the program.
-The two ways to write a comment are: //COMMENT /* COMMENT */
+func chapter1_exercise3() {
+	// Questions
+	fmt.Println("\n3. Our program began with package main. What would the files in the fmt package begin with?")
+	// Answer
+	fmt.Println("Ans.: Once the files in the fmt package use Golang, they must begin with package main.")
+}
 
-/*
-3. Our program began with package main. What would the files in the
-fmt package begin with?
-R. The files in the package must begin with package main too.
-*/
+func chapter1_exercise4() {
+	// Questions
+	fmt.Println("\n4. We used the Println function defined in the fmt package. If you wanted to use the Exit function from the os package, what would you need to do?")
+	// Answer
+	fmt.Println("Ans.: I would need to import the os package, then use the command os.Exit()")
+}
+
+func chapter1_exercise5() {
+	// Questions
+	fmt.Println("\n5. Modify the program we wrote so that instead of printing Hello, World it prints Hello, my name is followed by your name.")
+	// Answer
+	fmt.Println("Ans.: Hello, my name is Jean Carlo")
+}
